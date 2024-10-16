d685d002b5014e0e8b38cf1083219003dec61efe
Laires Pereira <lairespsoares@gmail.com>
2024-10-15 21:01:20 -0300

    add new fields to CreateUser

--- src/main/java/com/academicevents/api/handlers/CreateUser.java
+++ src/main/java/com/academicevents/api/handlers/CreateUser.java
@@ -13,17 +13,20 @@ public class CreateUser {
     public static void saveUser(User user) {
         Connection conn = DB.getConnection();
         String userType = user.getRole().getDisplayName();
-        String query = "INSERT INTO " + userType + " (cpf, nome, rua, numero, bairro, cidade, estado) " + "VALUES ( ?, ?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO " + userType + " (cpf, nome, email, senha, rua, numero, bairro, cidade, estado, role) " + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try {
             PreparedStatement statement =  conn.prepareStatement(query);
             statement.setString(1, user.getCpf());
             statement.setString(2, user.getName());
-            statement.setString(3, user.getRua());
-            statement.setString(4, user.getNumero());
-            statement.setString(5, user.getBairro());
-            statement.setString(6, user.getCidade());
-            statement.setString(7, user.getEstado());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getPassword());
+            statement.setString(5, user.getRua());
+            statement.setString(6, user.getNumero());
+            statement.setString(7, user.getBairro());
+            statement.setString(8, user.getCidade());
+            statement.setString(9, user.getEstado());
+            statement.setString(10, user.getRole().getDisplayName());
 
             statement.execute();
         } catch (SQLException e) {

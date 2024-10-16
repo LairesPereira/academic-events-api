17d351373e84f693011e9e2a0f8929567615a39d
Laires Pereira <lairespsoares@gmail.com>
2024-10-16 11:05:20 -0300

    created web config

--- src/main/java/com/academicevents/api/handlers/CreateUser.java
+++ src/main/java/com/academicevents/api/handlers/CreateUser.java
@@ -3,12 +3,14 @@ package com.academicevents.api.handlers;
 import com.academicevents.api.DAO.DB;
 import com.academicevents.api.models.User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 @Service
+@CrossOrigin(origins = "*")
 public class CreateUser {
     public static void saveUser(User user) {
         Connection conn = DB.getConnection();
